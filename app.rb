c66000857f7364de9f6388e560282bbe73708e8d
Mark Maglana <mmaglana@gmail.com>
2015-04-16 09:58:40 -0700

    Use kubernetes env variable for redis endpoint when available

--- app.rb
+++ app.rb
@@ -3,14 +3,31 @@ require 'redis'
 
 set :bind, '0.0.0.0'
 
+def redis_client
+    opts = {
+        :db => 'kubersample'
+    }
+
+    if ENV['REDIS_MASTER_SERVICE_HOST']
+        opts[:host] = ENV['REDIS_MASTER_SERVICE_HOST']
+        opts[:port] = ENV['REDIS_MASTER_SERVICE_PORT']
+    end
+
+    Redis.new(opts)
+end
+
 get '/' do
-    r = Redis.new
-    @messages = r.smembers "messages"
+    begin
+        r = redis_client
+        @messages = r.smembers "messages"
+    rescue Redis::CannotConnectError
+        @messages = []
+    end
     erb :index
 end
 
 post '/sign' do
-    r = Redis.new
+    r = redis_client
     r.sadd "messages", params['message']
     redirect '/'
 end
